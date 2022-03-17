6a08957de4dfde41f25f25b8f431958189287478
rumi-art <artyom1209rumi@gmail.com>
2022-03-17 09:37:58 -0400

    Initial Commit

---
+++ app/components/index.js
@@ -0,0 +1,8 @@
+export * from './bottom-tab';
+export * from './button';
+export * from './drawer';
+export * from './dropDownHolder';
+export * from './menu-header';
+export { default as PushNotification } from './push-notifications';
+export * from './text';
+export * from './touchable';
